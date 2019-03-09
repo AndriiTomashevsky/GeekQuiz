d5832760027c19191d4ceb554be0ecf1d803514c
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2019-03-09 18:59:47 +0200

    add angular

--- GeekQuiz/Scripts/app/quiz-controller.js
+++ GeekQuiz/Scripts/app/quiz-controller.js
@@ -1,5 +1,38 @@
 ﻿angular.module("QuizApp", []).controller("QuizCtrl", function ($scope, $http)
 {
-    $scope.answered = false;
+    $scope.options = [];
     $scope.title = "loading question...";
-})
+    $scope.answered = false;
+    $scope.isCorrectAnswer = false;
+    $scope.working = false;
+    $scope.answer = function ()
+    {
+        return $scope.isCorrectAnswer ? "correct" : "incorrect";
+    }
+
+    $scope.sendAnswer = function (option)
+    {
+        $scope.answered = true;
+
+        $http.post("/api/trivia", { "optionId": option.id, "questionId": option.questionId }).then(function success(response)
+        {
+            $scope.isCorrectAnswer = response.data;
+        }, function error(response)
+            {
+
+            });
+    };
+
+    $scope.nextQuestion = function ()
+    {
+        $http.get("/api/trivia").then(function success(response)
+        {
+            $scope.answered = false;
+            $scope.options = response.data.options;
+            $scope.title = response.data.title;
+        }, function error(response)
+            {
+                $scope.title = response.statusText;
+            });
+    };
+});
---
+++ GeekQuiz/Temp/JavaScript.js
@@ -0,0 +1,14 @@
+﻿angular.module('QuizApp', [])
+    .controller('QuizCtrl', function ($scope, $http)
+    {
+        $scope.answered = false;
+        $scope.title = "loading question...";
+        $scope.options = [];
+        $scope.correctAnswer = false;
+        $scope.working = false;
+
+        $scope.answer = function ()
+        {
+            return $scope.correctAnswer ? 'correct' : 'incorrect';
+        };
+    });
